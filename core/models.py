c7bf533a7af9f684d5b6ea46d1789358759ac4fc
Julio Matias <juliocesark2@hotmail.com>
2025-04-10 00:04:11 -0300

    feat: implement models: HealthProfessional and Appointment

--- core/models.py
+++ core/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+
+class HealthProfessional(models.Model):
+    social_name = models.CharField(max_length=255)
+    specialty = models.CharField(max_length=100)
+    phone = models.CharField(max_length=20, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.social_name
+    
+class Appointment(models.Model):
+    professional = models.ForeignKey(HealthProfessional, on_delete=models.CASCADE, related_name='appointments')
+    appointment_date = models.DateTimeField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'{self.professional} - {self.appointment_date.strftime("%Y-%m-%d %H:%M")}'
