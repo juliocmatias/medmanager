290eb19e3e0628b80eb434d55e31930bb2d72019
Julio Matias <juliocesark2@hotmail.com>
2025-04-10 00:45:56 -0300

    feat: implement viewsets for HealthProfessional and Appointment models

--- core/views.py
+++ core/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .models import HealthProfessional, Appointment
+from .serializers import HealthProfessionalSerializer, AppointmentSerializer
 
-# Create your views here.
+class HealthProfessionalViewSet(viewsets.ModelViewSet):
+    queryset = HealthProfessional.objects.all()
+    serializer_class = HealthProfessionalSerializer
+
+class AppointmentViewSet(viewsets.ModelViewSet):
+    queryset = Appointment.objects.all()
+    serializer_class = AppointmentSerializer
