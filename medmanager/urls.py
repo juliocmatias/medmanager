fc179c29885007bffdfc738934b8c090d438f4ed
Julio Matias <juliocesark2@hotmail.com>
2025-04-10 00:46:11 -0300

    feat: add API routing for HealthProfessional and Appointment viewsets

--- medmanager/urls.py
+++ medmanager/urls.py
@@ -15,8 +15,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from core.views import HealthProfessionalViewSet, AppointmentViewSet
+
+router = DefaultRouter()
+router.register(r'professionals', HealthProfessionalViewSet)
+router.register(r'appointments', AppointmentViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/', include(router.urls)),
 ]
